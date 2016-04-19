eccd4fcc9574e7cd930c2d981317916721c43645
Etienne Brouillard <etienne.brouillard@outlook.com>
2016-04-18 20:48:28 -0400

    Adding remember login

--- controllers/signin-controller.js
+++ controllers/signin-controller.js
@@ -6,9 +6,9 @@ var userService = require('../services/user-service.js'),
 var _viewName = 'signin',
     _redirectViewName = 'oauthRedirect';
 
-model.index = function(req, res, next) {
+model.index = function (req, res, next) {
     if (req.username) { return res.redirect('/'); }
-    var SignInModel = function() {
+    var SignInModel = function () {
         var model = new BaseModel(req, _viewName);
         model.csrfToken = req.csrfToken();
         model.returnUrl = req.query.returnUrl ? encodeURIComponent(req.query.returnUrl) : undefined;
@@ -18,20 +18,21 @@ model.index = function(req, res, next) {
     var vm = new SignInModel();
     res.render(_viewName, vm);
 };
-model.validate = function(req, res, next) {    
-    var SignInModel = function() {
+model.validate = function (req, res, next) {
+    var SignInModel = function () {
         var model = new BaseModel(req, _viewName);
         model.csrfToken = req.csrfToken();
         model.returnUrl = req.body.returnUrl;
         model.username = req.body.username;
+        model.rememberMe = req.body.rememberMe;
         return model;
     };
     var vm = new SignInModel();
-    userService.validate(req.body.username, req.body.password, function(err, result) {
+    userService.validate(req.body.username, req.body.password, function (err, result) {
         if (!err) {
             var returnParam = '';
             if (vm.returnUrl) { returnParam = '&returnUrl=' + encodeURIComponent(vm.returnUrl); }
-            res.redirect('/oauthredirect?token=' + result.token.token + '&expires=' + (moment(result.expires).toString() + returnParam));
+            res.redirect('/oauthredirect?token=' + result.token.token + '&expires=' + (moment(result.expires).toString() + returnParam) + '&keep=' + req.body.rememberMe);
         }
         else {
             vm.invalidLogin = true;
@@ -40,29 +41,34 @@ model.validate = function(req, res, next) {
         }
     });
 };
-model.oauthRedirect = function(req, res, next) {
-    var SignInModel = function() {
+model.oauthRedirect = function (req, res, next) {
+    var SignInModel = function () {
         var model = new BaseModel(req, _viewName);
         model.token = req.query.token;
         model.expires = req.query.expires;
         model.returnUrl = req.query.returnUrl;
         model.hasReturnUrl = (req.query.returnUrl && true);
+        model.rememberMe = req.query.keep;
         return model;
     };
     var vm = new SignInModel();
-    userService.validateToken(vm.token, function(err, result) {
+    userService.validateToken(vm.token, function (err, result) {
         if (!err && result) {
-            res.cookie('access_token', result.accessToken, { maxAge: 900000, httpOnly: true });
-            res.cookie('expires', result.expires, { maxAge: 900000, httpOnly: true });
+            var cookieOptions = { httpOnly: true };
+            if (req.query.keep === 'on') {
+                cookieOptions.maxAge = 90000000000;
+            }
+            res.cookie('access_token', result.accessToken,cookieOptions);
+            res.cookie('expires', result.expires, cookieOptions);
             return res.render(_redirectViewName, vm);
         }
         else { return res.render('/'); }
     });
 };
-model.signout = function(req, res, next) {
-    userService.signout(req.accessToken, function(err, result) {
-        res.cookie('access_token', '', { maxAge: 900000, httpOnly: true });
-        res.cookie('expires', '', { maxAge: 900000, httpOnly: true });
+model.signout = function (req, res, next) {
+    userService.signout(req.accessToken, function (err, result) {
+        res.cookie('access_token', '', { maxAge: 1, httpOnly: true });
+        res.cookie('expires', '', { maxAge: 1, httpOnly: true });
         res.redirect('/');
     });
 };
